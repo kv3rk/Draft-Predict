708ea480220239be60e28158de8008ac2f015351
kv3rk <kverk07@gmail.com>
2026-08-06 19:02:18 +0300

    created separated by page stats

--- src/main/java/lol/kv3rk/draft_predict/ClientApplication/Controller/ClientAppController.java
+++ src/main/java/lol/kv3rk/draft_predict/ClientApplication/Controller/ClientAppController.java
@@ -5,7 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/draft-predict")
@@ -21,20 +22,35 @@ public class ClientAppController {
         this.clientAppService = clientAppService;
     }
 
-    @GetMapping("/main")
-    public String getMainPage(Model model) throws InterruptedException {
-
-        log.info("Entered [/draft-predict/main] endpoint");
-
+    private void addCommonAttributes(Model model) {
         model.addAttribute("amountOfMatches", clientAppService.countMatches());
         model.addAttribute("actualPatch", clientAppService.actualPatch());
-        model.addAttribute("topPerformingChampions", clientAppService.getTopPerformingChampionsByPickRate());
-        model.addAttribute("mostBannedChampions", clientAppService.getMostBannedChampions());
         model.addAttribute("servers", String.join(", ", clientAppService.getRiotServerName()));
         model.addAttribute("tiers", String.join(", ", clientAppService.getTierParameters()));
         model.addAttribute("timeUpdated", clientAppService.lastTimeUpdate());
+    }
 
+    @GetMapping("/main")
+    public String getMainPage(Model model) {
+        log.info("Entered [/draft-predict/main] endpoint");
+        addCommonAttributes(model);
         return "main-page/main-page";
+    }
+
+    @GetMapping("/win-pick")
+    public String getWinPickPage(Model model) {
+        log.info("Entered [/draft-predict/win-pick] endpoint");
+        addCommonAttributes(model);
+        model.addAttribute("topPerformingChampions", clientAppService.getTopPerformingChampionsByPickRate());
+        return "stats-pages/top-perform/win-pick";
+    }
+
+    @GetMapping("/ban-rates")
+    public String getBanRatesPage(Model model) {
+        log.info("Entered [/draft-predict/ban-rates] endpoint");
+        addCommonAttributes(model);
+        model.addAttribute("mostBannedChampions", clientAppService.getMostBannedChampions());
+        return "stats-pages/top-ban/ban-rates";
     }
 
 
