5615166a51014b19e5934f07f7b8e0d48750342d
kv3rk <kverk07@gmail.com>
2026-08-10 23:47:28 +0300

    removed unnecessary library

--- src/main/java/lol/kv3rk/draft_predict/DefaultPipeline/Service/GatherMatchIDs.java
+++ src/main/java/lol/kv3rk/draft_predict/DefaultPipeline/Service/GatherMatchIDs.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
